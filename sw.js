6640841cc67ae842ebfc11d0f3db41c54ec22435
hojinra13-dotcom <hojinra13@gmail.com>
2026-09-01 22:07:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'electric-map-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './electric_map.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
